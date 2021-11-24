f37cde9aa68cb5c39011853d3bec864ffb80b3a6
gamalragab <jrjb079@gmail.com>
2021-11-24 17:19:46 +0200

    commit pr

--- src/helpers/ConnectionDataBase.kt
+++ src/helpers/ConnectionDataBase.kt
@@ -3,13 +3,9 @@ package com.example.helpers
 import org.ktorm.database.Database
 
 object ConnectionDataBase {
-   const val serverName="MYSQL5037.site4now.net"
-    const val dataBaseName="db_a7ce11_noteapp"
+
     val database = Database.connect(
-//        url = "jdbc:mysql://${serverName}/${dataBaseName}",
-//        driver = "com.mysql.cj.jdbc.Driver",
-//        user = "a7ce11_noteapp",
-//        password = "Gamal2172001"
+
 
         url = "jdbc:mysql://localhost:3306/notes",
         driver = "com.mysql.cj.jdbc.Driver",
